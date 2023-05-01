e64c82cf0e0e9f5e9c8af4387aedd3d8a672b681
Willmer Orsetti <112994224+willmerorsetti@users.noreply.github.com>
2023-05-01 10:47:31 -0600

    modifico StepDefinitions.java

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -2,20 +2,35 @@ package hellocucumber;
 
 import io.cucumber.java.en.*;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
     }
 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
+}
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Nope";
+    }
 }
