07d749a2b12a88164be509fa2b36464678a23b8b
Willmer Orsetti <112994224+willmerorsetti@users.noreply.github.com>
2023-05-01 10:55:57 -0600

    Revert "modifico StepDefinitions.java"
    This reverts commit e64c82cf0e0e9f5e9c8af4387aedd3d8a672b681.

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -2,35 +2,20 @@ package hellocucumber;
 
 import io.cucumber.java.en.*;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
-    private String today;
-    private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("an example scenario")
+    public void anExampleScenario() {
     }
 
-    @When("I ask whether it's Friday yet")
-    public void i_ask_whether_it_s_Friday_yet() {
-        actualAnswer = IsItFriday.isItFriday(today);
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
     }
 
-    @Then("I should be told {string}")
-    public void i_should_be_told(String expectedAnswer) {
-        assertEquals(expectedAnswer, actualAnswer);
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
     }
-}
 
-class IsItFriday {
-    static String isItFriday(String today) {
-        return "Nope";
-    }
 }
